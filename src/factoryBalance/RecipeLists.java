5d64d92fdd6c1e3d4f8d2e72c3b82c5ff3b9de16
treecal20 <treecal20@gmail.com>
2018-09-12 14:26:45 -0700

    Add recepies

--- src/factoryBalance/RecipeLists.java
+++ src/factoryBalance/RecipeLists.java
@@ -25,11 +25,15 @@ public class RecipeLists {
 		//	Name: Codes		Sheet: 0-1
 		Small[0][0][0]="Need Inputs";/**/Small[0][1][0]="No Input Needed";
 		Small[0][0][1]="0 2";        /**/Small[0][1][1]="1 2";
-		//		Science				Inserters					Electronics				Oil/Liquids
-		Small[1][0][0]="SPB";/**/Small[2][0][0]="IF1";/**/Small[3][0][0]="EB3";/**/Small[4][0][0]="OB1";/**/
-		Small[1][0][1]="2 3";/**/Small[2][0][1]="2 4";/**/Small[3][0][1]="2 5";/**/Small[4][0][1]="0 6";/**/
-		/*                 *//**//*                 *//**//*                 *//**/Small[4][1][0]="OP"; /**/
-		/*                 *//**//*                 *//**//*                 *//**/Small[4][1][1]="2 6";/**/
+		//		Science			 Inserters and Belts			Electronics				Oil/Liquids
+		Small[1][0][0]="SPR";/**/Small[2][0][0]="IF1";/**/Small[3][0][0]="EB3";/**/Small[4][0][0]="OB1";/**/
+		Small[1][0][1]="0 3";/**/Small[2][0][1]="2 4";/**/Small[3][0][1]="2 5";/**/Small[4][0][1]="0 6";/**/
+		Small[1][0][2]="SPG";/**//*                 *//**//*                 *//**/Small[4][1][0]="OP"; /**/
+		Small[1][0][3]="1 3";/**//*                 *//**//*                 *//**/Small[4][1][1]="2 6";/**/
+		Small[1][0][4]="SPB";/**//*                 *//**//*                 *//**//*                 *//**/
+		Small[1][0][5]="2 3";/**//*                 *//**//*                 *//**//*                 *//**/
+		
+		
 		
 	//	Type: Special				Code: 		Sheet: 2
 		//	Type: Need Inputs
@@ -42,15 +46,17 @@ public class RecipeLists {
 		
 	//	Type: Science				Code: S		Sheet: 3
 		//	Type: Red Science Pack		Code: SPR
-		
+		String[] SPR = {"1","12",/**/"IF1","1",/**/"EB3","1",/**/"OB1","1",/**/"OP","1"};
+		Small=CT3D(Small,SPR,0,3);/**/
 		//	Type: Green Science Pack	Code: SPG
-		
+		String[] SPG = {"1","12",/**/"IF1","1",/**/"EB3","1",/**/"OB1","1",/**/"OP","1"};
+		Small=CT3D(Small,SPG,1,3);/**/
 		//	Type: Blue Science Pack		Code: SPB
 		String[] SPB = {"1","12",/**/"IF1","1",/**/"EB3","1",/**/"OB1","1",/**/"OP","1"};
 		Small=CT3D(Small,SPB,2,3);/**/
 		
 		
-	//	Type: Inserters				Code: I		Sheet: 4
+	//	Type: Inserters and Belts		Code: I		Sheet: 4
 		//	Type: Inserter				Code: IR1
 		
 		//	Type: Fast Inserter			Code: IR1
